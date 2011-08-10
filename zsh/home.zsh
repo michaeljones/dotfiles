69893f310053b51e77c5a357bcd4899647357b28
michaeljones <m.pricejones@gmail.com>
2011-08-11 07:34:43 +1200

    Update to reflect new projects folder

--- zsh/home.zsh
+++ zsh/home.zsh
@@ -7,5 +7,5 @@ source ~/.user_config/zshrc
 export PATH="${PATH}:$HOME/local/git/bin:$HOME/projects/tools/bin"
 export PYTHONPATH="${PYTHONPATH}:${HOME}/local/lib/python2.6/site-packages"
 
-export TOOLS=$HOME/projects/tools
+export TOOLS=$HOME/root/production/tools
 export PKG_CONFIG_PATH=$TOOLS/lib/pkgconfig
